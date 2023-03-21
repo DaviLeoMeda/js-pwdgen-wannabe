fbaf4d1b24f467b74996024e165719dc26c96018
DaviLeoMeda <davileo.meda@gmail.com>
2023-03-21 15:52:29 +0100

    Completed

---
+++ assets/main.js
@@ -0,0 +1,14 @@
+let name = prompt('What is your name?');
+
+let surname = prompt('What is your surname?');
+
+let FavoriteColor = prompt('What is your favorite color?');
+
+document.writeln("<p>Ehi ciao " + name + " " + surname + ". Lo sai che per te una pessima password potrebbe essere " + name + surname + FavoriteColor + "? Ti garba?</p>");
+
+
+
+
+
+
+// ('<p>Ehi ciao ' + name + ' ' + surname + '.' + 'Una possibile pessima password che ho ensato per te potrebbe essere ' + name + surname + FavoriteColor + '. Ti è piaciuta?</p>);
